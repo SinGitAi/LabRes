425aab69e9a9f23868690199af85b933ed6cf4f9
Burkeq <daniilnovosletsev@gmail.com>
2024-04-08 16:10:07 +0300

    3 table in work 	modified:   lab.py

--- lab.py
+++ lab.py
@@ -1,5 +1,5 @@
 from prettytable import PrettyTable
-from math import log10
+from math import log10, sqrt
 # DATA = [51, 89 ,51 ,18, 51 ,81 ,30 ,39, 4 ,31 ,51, 26, 39, 51 ,78 ,74 ,11 ,48 ,16 ,76 ,57 ,32 ,99 ,69 ,94 ,40 ,53 ,46 ,73 ,14 ,78 ,16 ,60 ,94 ,12 ,60 ,48 ,15, 86 ,17 ,27 ,15 ,28 ,31 ,36 ,99 ,33 ,17 ,42,14]
 DATA = [1782, 1963, 1791, 1287, 1063, 1222, 1206, 1329, 1528, 1238, 1848, 1858, 1289, 1586, 772, 
         1266, 1242, 1816, 951, 1693, 1643, 1497, 1280, 1760, 1717, 1212, 1238, 1259, 1901, 1022, 
@@ -27,9 +27,25 @@ def second_table(data):
     sortedData = sorted(data)
     for i in range(0, len(sortedData) - 1):
         table.add_row([i+1, sortedData[i]])
-    print("Ранжированный ряд для велечины X\n(по увелечению X)")
-    print(table)
+    # print("Ранжированный ряд для велечины X\n(по увелечению X)")
+    # print(table)
     return sortedData
+
+def find_divisors(n):
+
+    divisors = []
+
+    for i in range(1, int(sqrt(n))+1):
+
+        if n % i == 0:
+
+            divisors.append(i)
+
+            if i != n // i:
+
+                divisors.append(n // i)
+
+    return divisors
 
 def third_table(data):
     list_poryad_nomer = []
@@ -64,7 +80,8 @@ def third_table(data):
 def interval_calculation(*, min_value, max_value, count_elements):
     scope_of_ineterval = max_value - min_value
     size_of_ineterval = int((scope_of_ineterval)/(1 + 3.28*log10(count_elements)))
-    print(scope_of_ineterval, size_of_ineterval)
+    print(interval_step_check(size_of_ineterval, second_table(DATA), scope_of_ineterval))
+    
 
 def check_step(size_of_ineterval, elements):
     count = 146
@@ -75,28 +92,17 @@ def check_step(size_of_ineterval, elements):
         count_of_steps+=1
     return count > max_element, count, count_of_steps #Если шаг больше то возвращает true
     
-def interval_step_check(size_of_ineterval, elements):
-    # mass_of_intervals = []
-    # item_number = 0
-    # count = min(elements)
-    # while(count <= max(elements)):
-    #     count_prev = count
-    #     count += size_of_ineterval
-    #     mass_of_intervals.append(f"{count_prev} - {count}")
-    #     print(mass_of_intervals)
-    while(check_step(size_of_ineterval, elements)):
-        
-        
-    # correct_size_interval = 
-# print(max(DATA), min(DATA))
-# interval_calculation(max_value=max(DATA), min_value = min(DATA), count_elements=len(DATA))
-
+def interval_step_check(size_of_ineterval, elements, scope_of_ineterval):
+    correct_size_interval = size_of_ineterval
+    if(check_step(correct_size_interval, elements)):
+        divisors = find_divisors(scope_of_ineterval)
+        i = 0
+        while(True):
+            correct_size_interval = divisors[i]
+            if(not check_step(correct_size_interval, elements)):
+                return correct_size_interval
+    else:
+        return correct_size_interval
     
     
-interval_step_check(270, second_table(DATA))
-    
-    
-    
-    
-# first_table(DATA)
-# second_table(DATA)
+interval_calculation(min_value=min(DATA), max_value=max(DATA), count_elements=len(DATA))
